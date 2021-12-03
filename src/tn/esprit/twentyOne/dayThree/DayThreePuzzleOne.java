1f705cdec8576388aab10588cd61505c84bbb3a0
chihabhajji <blackpang23@gmail.com>
2021-12-03 16:33:22 +0100

    Day 3 ✔️🙌

--- src/tn/esprit/twentyOne/dayThree/DayThreePuzzleOne.java
+++ src/tn/esprit/twentyOne/dayThree/DayThreePuzzleOne.java
@@ -8,12 +8,37 @@ import java.io.IOException;
 
 public class DayThreePuzzleOne {
     public static void main(String[] args) {
-        int x = 0, y = 0, i = 0 , j = 0;
+        // CANT I SOLVE THIS USING HASH ?
+        String gammaRate = "", epsilonRate = "";
+        int[][] diagnostics = new int[1000][12];
         String line;
+        int i = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(PathToFile.convertActual("dayThree.txt")))) {
             while ((line = br.readLine()) != null) {
-                Integer.parseInt(line);
+                char[] chararray = line.toCharArray();
+                for(int j = 0; j< 12; j++){
+                    diagnostics[i][j] = Integer.parseInt(String.valueOf(chararray[j]));
+                }
+                i++;
             }
+            for (int j = 0; j < 12; j++) {
+                int zeros = 0, ones = 0;
+                for (int k = 0; k < 1000; k++) {
+                    if(diagnostics[k][j] == 0)
+                        zeros ++;
+                    else
+                        ones ++;
+                }
+                if (zeros > ones){
+                    gammaRate += "0";
+                    epsilonRate += "1";
+                }else{
+                    gammaRate += "1";
+                    epsilonRate += "0";
+                }
+            }
+            System.out.println(Long.parseLong(gammaRate, 2) * Long.parseLong(epsilonRate, 2));
+            // 3923414
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/tn/esprit/twentyOne/dayThree/DayThreePuzzleTwo.java
+++ src/tn/esprit/twentyOne/dayThree/DayThreePuzzleTwo.java
@@ -5,17 +5,56 @@ import tn.esprit.helpers.PathToFile;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DayThreePuzzleTwo {
+    static final StringBuilder oxygen = new StringBuilder();
+    static final StringBuilder co2 = new StringBuilder();
     public static void main(String[] args) {
-        int x = 0, y = 0, i = 0 , j = 0;
+        int zeros = 0;
+        int ones = 0;
         String line;
+        Set<String> remainingOxygen = new HashSet<>();
+        Set<String> remainingCo2 = new HashSet<>();
         try (BufferedReader br = new BufferedReader(new FileReader(PathToFile.convertActual("dayThree.txt")))) {
             while ((line = br.readLine()) != null) {
-                Integer.parseInt(line);
+                if(line.charAt(0) == '0') zeros++; else ones++;
+                remainingOxygen.add(line);
+                remainingCo2.add(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Object o = checkForRemaining(remainingOxygen, oxygen, 1, zeros, ones,'O');
+        Object c = checkForRemaining(remainingCo2, co2, 1, zeros , ones, 'C');
+        System.out.println(Long.parseLong(c.toString(), 2) * Long.parseLong(o.toString(), 2));
+        // 5852595
     }
+
+    static Object checkForRemaining(Set<String> set, StringBuilder prefixBuilder, int i, long zeros , long ones, char o) {
+        if(set.size() == 1){
+            return set.toArray()[0];
+        } else {
+            if(zeros > ones){
+                if(o == 'O')
+                    prefixBuilder.append("0");
+                else
+                    prefixBuilder.append("1");
+            } else  {
+                if(o == 'C')
+                    prefixBuilder.append("0");
+                else
+                    prefixBuilder.append("1");
+            }
+            set = set.stream().filter(x -> x.startsWith(prefixBuilder.toString())).collect(Collectors.toSet());
+            if(i<12){
+                zeros = set.stream().filter(s -> s.charAt(i) == '0').count();
+                ones = set.stream().filter(s -> s.charAt(i) == '1').count();
+            }
+            return checkForRemaining(set,prefixBuilder,i+1, zeros, ones,o);
+        }
+    }
+
 }
+
